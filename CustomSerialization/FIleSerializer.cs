b8193d2881678790989cd115122d21193bc32205
sator <sator@SATOR-PC>
2016-11-30 19:55:36 +0200

    Custom serialize

---
+++ CustomSerialization/FIleSerializer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomSerialization
+{
+    public static class FileSerializer
+    {
+        public static void Serialize(string filename, object objectToSerialize)
+        {
+            if (objectToSerialize == null)
+                throw new ArgumentNullException("objectToSerialize cannot be null");
+            Stream stream = null;
+            try
+            {
+                stream = File.Open(filename, FileMode.Create);
+                BinaryFormatter bFormatter = new BinaryFormatter();
+                bFormatter.Serialize(stream, objectToSerialize);
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+        }
+
+        public static T Deserialize<T>(string filename)
+        {
+            T objectToSerialize = default(T);
+            Stream stream = null;
+            try
+            {
+                stream = File.Open(filename, FileMode.Open);
+                BinaryFormatter bFormatter = new BinaryFormatter();
+                objectToSerialize = (T)bFormatter.Deserialize(stream);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("The application failed to retrieve the inventory - " + err.Message);
+            }
+
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+            return objectToSerialize;
+        }
+    }
+}
---
+++ CustomSerialization/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomSerialization
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            var productList = new List<Product>
+            {
+                new Product("Колбаса", DateTime.Today, new DateTime(2017, 3, 24)),
+                new Product("Хлеб", new DateTime(2016, 11, 29), DateTime.Today),
+                new Product("Молоко", new DateTime(2016, 10, 14), new DateTime(2016, 10, 20))
+            };
+            List<Product> deDerializeList;
+
+            ListPresenter(productList);
+
+            FileSerializer.Serialize(@"Products.dat", productList);
+
+            deDerializeList = FileSerializer.Deserialize<List<Product>>(@"Products.dat");
+            ListPresenter(deDerializeList);
+            Console.ReadLine();
+        }
+
+        static void ListPresenter<T>(List<T> list) where T : class
+        {
+            foreach (var obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+
+        
+    }
+}
