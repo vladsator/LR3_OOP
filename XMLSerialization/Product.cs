d27ac350dad5adcbdb2895a71d44608f18cff667
sator <sator@SATOR-PC>
2016-11-30 19:12:14 +0200

    1

---
+++ XMLSerialization/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace XMLSerialization
+{
+    [Serializable]
+    public class Product
+    {
+        private static int _objCount;        public int Id { get; }
+        public string Name { get;}
+        public DateTime ManufactureDate { get; }
+        public DateTime ExpirationDate { get; } 
+
+        public Product()
+        {
+            
+        }
+
+        public Product(string name, DateTime manufactureDate, DateTime expirationDate)
+        {
+            Id = _objCount++;
+            Name = name;
+            ManufactureDate = manufactureDate;
+            if (expirationDate > ManufactureDate)
+            {
+                ExpirationDate = expirationDate;
+            }
+            else throw new ArgumentException("Expiration date < Manufacture date");
+        }
+
+        public bool PossibleToEat()
+        {
+            return ExpirationDate >= DateTime.Today;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("ID = {0} Name = {1}\nManufacture date = {2:d} Expiration date = {3:d}\nPossible to eat = {4}\n", Id, Name, ManufactureDate, ExpirationDate, PossibleToEat());
+        }
+    }
+}
---
+++ XMLSerialization/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLSerialization
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            var productList = new List<Product>
+            {
+                new Product("Колбаса", DateTime.Today, new DateTime(2017, 3, 24)),
+                new Product("Хлеб", new DateTime(2016, 11, 29), DateTime.Today),
+                new Product("Молоко", new DateTime(2016, 10, 14), new DateTime(2016, 10, 20))
+            };
+            List<Product> deDerializeList = null;
+
+            ListPresenter(productList);
+
+            var formatter = new XmlSerializer(typeof(List<Product>));
+            using (var fs = new FileStream("Product.xml",FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, productList);
+            }
+
+            //using (var fs = new FileStream("Product.xml", FileMode.OpenOrCreate))
+            //{
+            //    deDerializeList = (List<Product>) formatter.Deserialize(fs);
+            //}
+            //ListPresenter(deDerializeList);
+            Console.ReadLine();
+        }
+
+        static void ListPresenter<T>(List<T> list) where T : class
+        {
+            foreach (var obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
