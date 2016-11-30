d84dd8e2fee643234065ff065e2285d356e0c992
sator <sator@SATOR-PC>
2016-11-30 16:11:04 +0200

    main code

--- LR_3_code_Cch/Program.cs
+++ LR_3_code_Cch/Program.cs
@@ -10,6 +10,22 @@ namespace LR_3_code_Cch
     {
         static void Main(string[] args)
         {
+            var productList = new List<Product>();
+
+            var addingProduct = new Product("Колбаса", DateTime.Today, new DateTime(2017, 3, 24));            
+            productList.Add(addingProduct);
+
+            addingProduct = new Product("Хлеб", new DateTime(2016, 11, 29), DateTime.Today);
+            productList.Add(addingProduct);
+
+            addingProduct = new Product("Молоко", new DateTime(2016, 10, 14), new DateTime(2016, 10, 20));
+            productList.Add(addingProduct);
+
+            foreach (var product in productList)
+            {
+                Console.WriteLine(product);    
+            }
+            Console.ReadLine();
         }
     }
 }
