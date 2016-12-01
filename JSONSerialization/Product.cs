2103c0014ddaa9b1aa5742dfcca0541502738cef
sator <sator@SATOR-PC>
2016-12-01 11:17:12 +0200

    Json serialize

---
+++ JSONSerialization/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace JSONSerialization
+{
+    [DataContract]
+    class Product
+    {
+        private static int _objCount;
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public DateTime ManufactureDate { get; set; }
+        [DataMember]
+        public DateTime ExpirationDate { get; set; }
+
+        public Product(string name, DateTime manufactureDate, DateTime expirationDate)
+        {
+            Id = _objCount++;
+            Name = name;
+            ManufactureDate = manufactureDate;
+            if (expirationDate > ManufactureDate)
+            {
+                ExpirationDate = expirationDate;
+            }
+            else throw new ArgumentException("Expiration date < Manufacture date");
+        }
+
+        public bool PossibleToEat()
+        {
+            return ExpirationDate >= DateTime.Today;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("ID = {0} Name = {1}\nManufacture date = {2:d} Expiration date = {3:d}\nPossible to eat = {4}\n", Id, Name, ManufactureDate, ExpirationDate, PossibleToEat());
+        }
+    }
+}
---
+++ JSONSerialization/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONSerialization
+{
+    class Program
+    {
+        static void Main()
+        {
+            var productList = new List<Product>
+            {
+                new Product("Колбаса", DateTime.Today, new DateTime(2017, 3, 24)),
+                new Product("Хлеб", new DateTime(2016, 11, 29), DateTime.Today),
+                new Product("Молоко", new DateTime(2016, 10, 14), new DateTime(2016, 10, 20))
+            };
+            List<Product> deDerializeList;
+
+            ListPresenter(productList);
+
+            var jsonformatter = new DataContractJsonSerializer(typeof(List<Product>));
+
+            using (FileStream fs = new FileStream("Product.json", FileMode.OpenOrCreate))
+            {
+                jsonformatter.WriteObject(fs, productList);
+            }
+
+            using (FileStream fs = new FileStream("Product.json", FileMode.OpenOrCreate))
+            {
+                deDerializeList = (List<Product>)jsonformatter.ReadObject(fs);
+            }
+
+            ListPresenter(deDerializeList);
+            Console.ReadLine();
+        }
+
+        static void ListPresenter<T>(List<T> list) where T : class
+        {
+            foreach (var obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
